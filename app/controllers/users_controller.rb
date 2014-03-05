25c81a49dfe3b4e8b3b327528a38a8761efa7efc
mwatkin3 <Michael@Michaels-Laptop>
2014-03-04 19:05:52 -0500

    Edited user controller and uncommented some code messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,13 +29,13 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-       # format.html { redirect_to @user, notice: 'User was successfully created.' }
-       # format.json { render action: 'show', status: :created, location: @user }
-        flash[:success] = "Welcome to the Sample App!"
+       format.html { redirect_to @user, notice: 'User was successfully created.' }
+       format.json { render action: 'show', status: :created, location: @user }
+        flash[:success] = "Welcome to the SentenceZing!"
         redirect_to @user
       else
-       # format.html { render action: 'new' }
-       # format.json { render json: @user.errors, status: :unprocessable_entity }
+       format.html { render action: 'new' }
+       format.json { render json: @user.errors, status: :unprocessable_entity }
        render 'new'
       end
     end
