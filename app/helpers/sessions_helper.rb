a4b3b7f626cfaa66c10402031ccd23def697cabb
bhighnam <bhighnam@gmail.com>
2014-03-11 16:43:02 -0400

    Completed hartl listing 8.19

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,9 @@
 module SessionsHelper
-end
+   def sign_in(user)
+    remember_token = User.new_remember_token
+    cookies.permanent[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.hash(remember_token))
+    self.current_user = user
+
+  end # end of method sign_in( user )
+end # end of module SessionHelper
