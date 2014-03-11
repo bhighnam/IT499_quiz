f1e0f2968707ffda4e1053e1ba3e2e9b02b4c4ac
mwatkin3 <Michael@Michaels-Laptop.home>
2014-03-11 16:47:18 -0400

    Hartl Listing 8.22

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,9 +1,18 @@
 module SessionsHelper
    def sign_in(user)
-    remember_token = User.new_remember_token
-    cookies.permanent[:remember_token] = remember_token
-    user.update_attribute(:remember_token, User.hash(remember_token))
-    self.current_user = user
+      remember_token = User.new_remember_token
+      cookies.permanent[:remember_token] = remember_token
+      user.update_attribute(:remember_token, User.hash(remember_token))
+      self.current_user = user
+    end # end of method sign_in( user )
+    
+    def current_user=(user)
+      @current_user = user
+    end
+    
+    def current user
+      rememeber_token = User.hash(cookies[:remember_token])
+      @current_user ||= User.find_by(remember_token: remember_token)
+    end
 
-  end # end of method sign_in( user )
 end # end of module SessionHelper
