bcfea975ad0eea311419bd2a05c35eabe4371cf8
bhighnam <bhighnam@gmail.com>
2014-03-17 15:47:26 -0400

    Did Hartl listing 9.19

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -73,7 +73,12 @@ class UsersController < ApplicationController
     end # end of method user_params
 
     def signed_in_user
-        redirect_to signin_url, notice: "Please sign in." unless signed_in?
+
+       unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end # end of unless signed_in?
+
     end # end of method signed_in_user
     
     def correct_user
