ab5930a0f9205aa9b2d624edf4fe4941e0dae60f
mwatkin3 <Michael@Michaels-Laptop.home>
2014-03-17 11:37:08 -0400

    Changes to private section of users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,13 +34,6 @@ class UsersController < ApplicationController
         render 'new'
       end
     end
-    
-    private
-
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
   end
 
   # PATCH/PUT /users/1
@@ -74,9 +67,7 @@ class UsersController < ApplicationController
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-      def user_params
-         params.require(:user).permit(:email, :password)
-      end
-  
-
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end # end of class UserController 
