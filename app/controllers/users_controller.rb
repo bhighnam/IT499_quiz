4ec573fb4019735132ae2c5267a6624425e31bb5
mwatkin3 <Michael@Michaels-Laptop.home>
2014-03-17 15:39:10 -0400

    Hartl Listing 9.14, 9.15, 9.17

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
 
   # GET /users
   # GET /users.json
@@ -74,4 +75,9 @@ class UsersController < ApplicationController
     def signed_in_user
         redirect_to signin_url, notice: "Please sign in." unless signed_in?
     end # end of method signed_in_user
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end # end of class UserControl 
