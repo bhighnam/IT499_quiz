b959c5ae558b3b2d96e5b09e3dac78c01969ef28
mwatkin3 <Michael@Michaels-Laptop.home>
2014-03-17 13:45:10 -0400

    Changed validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,12 @@ class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
   before_create :create_remember_token
   
-  validates :email, :password, :presence => true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, format: { with: VALID_EMAIL_REGEX}
-  validates :email, :uniqueness => true
-  validates :password, :length => {:minimum => 6}
+  validates :email, presence:   true,
+                    format:     { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, length: { minimum: 6 }
   has_many :microposts
   
   def User.new_remember_token
